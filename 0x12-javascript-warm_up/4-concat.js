a5ba59733a77a8527dc157b2d3e5fc09fb7fbbff
Comfort Ajayi <ajayicomfort08@gmail.com>
2023-04-12 09:49:30 -0700

    4-concat

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Script to print two arguments passed in the specified format
+// Get the first two arguments
+// Print the arguments in the specified format
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+console.log(arg1 + ' is ' + arg2);
