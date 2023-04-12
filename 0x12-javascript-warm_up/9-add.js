e71ab6c941f856ddddb0aeb4181ee0d3f6e21fe1
Comfort Ajayi <ajayicomfort08@gmail.com>
2023-04-12 12:30:48 -0700

    9-add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Define the add function
+// Perform the addition
+// Print the output
+// Get the command line arguments
+// Parse the first argument as an integer
+// Parse the second argument as an integer
+// Call the add function with the parsed arguments
+
+const add = (a, b) => {
+  const sum = a + b;
+  console.log('The sum is:', sum);
+};
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+add(arg1, arg2);
