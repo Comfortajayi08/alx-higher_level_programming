4b4857cc3daed9eb2d422018a3d4d09ea6a97e8e
Comfort Ajayi <ajayicomfort08@gmail.com>
2023-04-12 08:04:30 -0700

    1-multi

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Script to print three lines using console.log() in JavaScript.
+
+const firstLine = 'C is fun';
+console.log(firstLine);
+const secondLine = 'Python is cool';
+console.log(secondLine);
+const thirdLine = 'JavaScript is amazing';
+console.log(thirdLine);
