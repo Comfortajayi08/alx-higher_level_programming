fac05ca36b51299a11e5a31f9c2df9c76c285f35
Comfort Ajayi <ajayicomfort08@gmail.com>
2023-04-28 07:05:17 -0700

    network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Check if an argument was provided
-if [ -z "$1" ]; then
-    echo "Please provide a URL"
-    exit 1
-fi
-
-# Make the request and store the response body in a variable
-response=$(curl -s -w '%{size_download}' -o /dev/null "$1")
-
-# Display the size of the response body in bytes
-echo "The size of the response body is $response bytes."
+# ends a request to that URL displays the size of the response body
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
